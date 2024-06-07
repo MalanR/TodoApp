94d0e85a7f1910c97ffad65a0837b4efff4139ea
Malan <102535792+MalanR@users.noreply.github.com>
2024-06-07 14:21:49 +0200

    fixed names of methods

--- my_venv/src/TodoApp.py
+++ my_venv/src/TodoApp.py
@@ -39,13 +39,13 @@ def load_tasks():
 
 # Defining the TodoList class and initializing it by loading existing tasks
 # Todo Change todoApp
-class TodoList:
+class TodoApp:
     # loading the existing tasks from the json file to the todolist
 
     @staticmethod
     # use new to start a new blank todo list from scratch.
     def new():
-        return TodoList()
+        return TodoApp()
 
     def __init__(self):
         self.todos = load_tasks()
@@ -56,8 +56,8 @@ class TodoList:
         for task_id, task in self.todos.items():
             print(f"ID: {task_id}")
             print(f"Description: {task['description']}")
-            print(f"Created at: {task['created_at']}")
-            print(f"Completed at: {task['completed_at']}")
+            print(f"Created_on: {task['created_on']}")
+            print(f"Completed_on: {task['completed_on']}")
             print("--------------------------------------------------------------------")
 
     async def user_action(self):
@@ -66,19 +66,19 @@ class TodoList:
         # created as a while loop so that this "menu" displays after each action the user makes.
 
             crud_operations = {
-                1: self.add_task,
-                2: self.delete_task,
-                3: self.update_task,
-                4: self.complete_task,
+                1: self.create,
+                2: self.delete,
+                3: self.update,
+                4: self.complete,
                 5: self.print_todos,
-                6: self.search_task,
+                6: self.filter,
                 7: lambda: print("Goodbye") or exit()
             }
 
             try:
                 user_input = int(input(
                     "1)\033[92m Add Task\033[0m\n2)\033[92m Delete a task\033[0m\n3)\033[92m Update a task\033["
-                    "0m\n4)\033[92m Complete a task\033[0m\n5)\033[92m View"
+                    "0m\n4)\033[92m Complete a task\033[0m\n5)\033[92m View "
                     "all tasks\033[0m\n6)\033[92m Filter tasks\033[0m\n7)\033[93m Exit\033[0m\nChoose an option: "))
 
                 # Checking for a match in the user input and the crud dictionary.
@@ -90,7 +90,7 @@ class TodoList:
                         # get the task name/description from the user.
                         description = input("Enter task description: ")
 
-                        # Await the add_task function to execute.
+                        # Await the create function to execute.
                         await crud_operations[user_input](description)
 
                     #     check if the user input is 2,3 or 4, these require an existing tasks ID
@@ -105,7 +105,7 @@ class TodoList:
                             # get the new task name/description from the user
                             task_description = input("Enter new task description: ")
 
-                            # Await the update_task function to execute.
+                            # Await the update function to execute.
                             await crud_operations[user_input](task_id, task_description)
                         else:
 
@@ -122,7 +122,7 @@ class TodoList:
                         # take in the users choice.
                         search_choice = input('Please choose a search option: ')
 
-                        # Await the search_task function to execute.
+                        # Await the filter function to execute.
                         await crud_operations[user_input](search_choice)
                     else:
 
@@ -141,7 +141,7 @@ class TodoList:
                 # ValueError incase the user's input is not an integer.
                 print("\033[91mYou did not enter an integer\033[0m")
 
-    async def add_task(self, description):
+    async def create(self, description):
         # generating an task ID for each task by using UUID.
         task_id = str(uuid.uuid4())
 
@@ -149,8 +149,8 @@ class TodoList:
         new_task = {
             "id": task_id,
             "description": description,
-            "created_at": datetime.datetime.now(),
-            "completed_at": None
+            "created_on": datetime.datetime.now().strftime("%d-%H:%M"),
+            "completed_on": None
         }
 
         #  adding the new task to the todolist dictionary.
@@ -164,7 +164,7 @@ class TodoList:
         print(f"Task '{description}' added with ID {task_id}")
         return task_id, description
 
-    async def delete_task(self, task_id):
+    async def delete(self, task_id):
         # one the task with the provided ID was found we simply delete it from the json file.
         if task_id in self.todos:
             del self.todos[task_id]
@@ -178,7 +178,7 @@ class TodoList:
             print(f"Task with ID {task_id} not found. ")
 
     #  update the description of a task after finding the task ID
-    async def update_task(self, task_id, task_description):
+    async def update(self, task_id, task_description):
 
         # finding the task by its ID
         if task_id in self.todos:
@@ -193,13 +193,13 @@ class TodoList:
         else:
             print(f"Task with ID {task_id} not found. ")
 
-    async def complete_task(self, task_id):
+    async def complete(self, task_id):
 
         # finding the task by its ID
         if task_id in self.todos:
 
             # finding the tasks completed at status and setting it to be equal to the current datetime.
-            self.todos[task_id]['completed_at'] = datetime.datetime.now()
+            self.todos[task_id]['completed_on'] = datetime.datetime.now()
 
             # update json file
             store_tasks(self.todos)
@@ -207,7 +207,7 @@ class TodoList:
         else:
             print("Invalid task ID")
 
-    async def search_task(self, user_input):
+    async def filter(self, user_input):
         # create a list to keep the filtered tasks in for when we return them to the user.
         filtered_tasks = []
 
@@ -218,14 +218,14 @@ class TodoList:
             for task_id, task in self.todos.items():
 
                 # finding all the tasks with a completed at state of None.
-                if task['completed_at'] is None:
+                if task['completed_on'] is None:
                     # adding the found tasks to the filtered tasks list.
                     filtered_tasks.append(task)
         elif user_input == '2':
             for task_id, task in self.todos.items():
 
                 # finding the tasks with a status that is not None.
-                if task['completed_at'] is not None:
+                if task['completed_on'] is not None:
                     # adding the found tasks to the filtered tasks list.
                     filtered_tasks.append(task)
         elif user_input == '3':
@@ -250,14 +250,14 @@ class TodoList:
             print(" ")
             print(f"ID: {task['id']}"),
             print(f"Description: {task['description']}"),
-            print(f"Created at: {task['created_at']}"),
-            print(f"Completed at: {task['completed_at']}"),
+            print(f"Created_on: {task['created_on']}"),
+            print(f"Completed_on: {task['completed_on']}"),
             print("--------------------------------------------------------------------")
         return filtered_tasks
 
 
 # create a new instance of the TodoList by using the static factory method.
-my_todo_list = TodoList.new()
+my_todo_list = TodoApp.new()
 
 # run the user_action method to be able to interact with the todolist.
 asyncio.run(my_todo_list.user_action())
